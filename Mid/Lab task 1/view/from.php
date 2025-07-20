6ac525aa16b93c9803f277113ffd13e8e4afd6f1
minhajul21 <147337273+minhajul21@users.noreply.github.com>
2025-07-21 00:18:31 +0600

    Add files via upload

---
+++ Mid/Lab task 1/view/from.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTE-8">
+    <meta name="viewport" content="width=device-width, initial scale =1.0">
+    <title<Task1></title>
+</head>
+<body>
+  <center>
+    <h1 style ="color : blue ">AIUB</h1>
+    <h2 style ="color : blue ">Registration From </h2>
+
+</center>
+
+<table>
+  <tr>
+    <td><p><b>Complete The Registration</b></p></td>
+</tr>
+<tr>
+  <td>Full Name :</td>
+  <td><input type ="text" name=" "placeholder ="Enter Full Name" id=" "></tb>
+</tr>
+<tr>
+  <td> Email :</td>
+  <td><input type ="email" name=" " placeholder =" Enter Email"id=" "></td>
+</tr>
+<tr>
+  <td>password :</td>
+  <td<input type="password" name=" " placeholder =" Enter password" id=" "></tb>
+  <tr>
+    <td>Gender :</td>
+    <td><input type="radio"name="des">Male</tr>
+    <td><input type="radio"name="des">other</tr>
+</tr>
+
+<tr>
+  <tr>
+    <td>Language Knowledge:</td>
+    <td><input type =" checkbox" name="des">English</td>
+    <td><input type ="checkbox" name="des">Bangla</td>
+    <td><input type ="checkbox" name ="des"<Hindi</td>
+</tr>
+<tr>
+  <td> Country : </td>
+  <td><select>
+    <option value =" "> Bnagladesh</option>
+    <option value =" "> India</option>
+    <option value =" "> Pakistan</option>
+</select ></td>
+</tr>
+<tr>
+  <td> Date of Birth </td>
+  <td> <input type ="date"></td>
+</tr>
+<tr>
+  <td>Upload Phone: </td>
+  <td> <input type = "File"></td>
+</tr>
+
+<tr>
+  <tr></td>
+  <td>Comment:</td>
+  <td>
+    <textarea id="comment" name= "comment" cols="35" rows="7"> </textarea>
+  </td>
+</td>
+</tr>
+  <tr>
+ <td></td>
+ <td>
+  <button>Register</button>
+</td>
+</tr>
+
+</table>
+
+</body>
+</html>
