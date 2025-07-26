80ba4a8224df93e8ec491211aa0f1c98cd5b3bc2
minhajul21 <kminhajul55@gmail.com>
2025-07-26 23:32:46 +0600

    updated

---
+++ Mid/Lab_task1/from.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <center>
+      <h1 style ="color:blue">AIUB </h1>
+      <h2 style ="color:blue">Registration Form </h2>
+</center>
+<table>
+  <tr>
+    <td><p><b>Complete The Registration</b></p></td>
+</tr>
+<tr>
+<td>Fullname:</td>
+<td><input type="text" placeholder ="Enter Full Name" id=" "></td>
+</tr>
+<tr>
+  <td>Email:</td>
+  <td><input type="email" name="" placeholder ="Enter Email "id=""></td>
+</tr>
+<tr>
+  <td>Password:</td>
+  <td><input type ="password" name= " " placeholder ="Enter password" id =" "></td>
+</tr>
+<tr>
+  <td>Gender:</td>
+  
+    <td><input type="radio" name="des">Male</td>
+    <td><input type="radio" name="des">Other </td>
+</tr>
+<tr>
+  <td>Language Known:</td>
+  <td>
+    <input type="checkbox" name="des">English
+    <input type="checkbox" name="des">Bangla
+    <input type="checkbox" name="des">Hindi
+</td>
+</tr>
+<tr>
+  <td> Country : </td>
+  <td><select>
+    <option value =" "> Bangladesh</option>
+    <option value =" "> India</option>
+    <option value =" "> Pakistan</option>
+</select ></td>
+</tr>
+<tr>
+  <td> Date of Birth </td>
+  <td> <input type ="date"></td>
+</tr>
+<tr>
+  <td>Upload Phone:</td>
+  <td><input type ="File"></td>
+</tr>
+<tr>
+  <tr></td>
+  <td>comment:</td>
+  <td>
+    <textarea id="comment" name= "comment" cols="35" rows="7"></textarea>
+</td>
+</tr>
+<tr>
+ <td></td>
+ <td>
+  <button>Register</button>
+</td>
+</tr>
+</table>
+</body>
+</html>
+
