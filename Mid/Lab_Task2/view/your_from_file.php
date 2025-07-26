7f81ce1a7f74dc5264b533b4cf0fe3dada416db5
minhajul21 <kminhajul55@gmail.com>
2025-07-26 21:18:25 +0600

    Initial commit

---
+++ Mid/Lab_Task2/view/your_from_file.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+  <title>Bank Managment System</title>
+  <style>
+    .div{
+      backgroud-color-white;
+      width: 190px;
+      border:1px solid red;
+      overflow:scroll;
+    }
+    </style>
+    <head>
+      <body>
+        <center>
+          <h1>Bank Management System</h1>
+          <h2>Your Trusted Financial Partner</h2>
+  </center>
+<tr>
+  <td><p><b>Customer Registration From</b></p></td>
+  </tr>
+  <table>
+    <from action="">
+      <tr>
+        <td>Full Name:</td>
+        <td><input type="text" name="" placeholder="Enter Full Name"id=""></td>
+  </tr>
+  <tr>
+    <td>Date of Birth:</td>
+    <td><input type ="date"></td>
+  </tr>
+  <tr>
+    <td>Gender:</td>
+    <td>
+      <input type="radio" name ="des">Male
+      <input type="radio" name ="des">Female
+      <input type="radio" name ="des">Other
+  </td>
+  </tr>
+  <tr>
+    <td>Marital Status:</td>
+    <td>
+      <select>
+        <option value="">Single</option>
+        <option value="">Married</option>
+  </select>
+  </td>
+  </tr>
+   <tr>
+    <td>Account Type:</td>
+    <td>
+      <select>
+        <option value ="">Savings</option>
+        <option value="">Fixed</option>
+  </select>
+  </td>
+  </tr>
+  <tr>
+    <td>Initial Desposit Amount:</td>
+    <td><input type="text"placeholder="Enter amount"
+  </tr>
+  <tr>
+    <td>Mobile Number:</td>
+    <td><input type="text" placeholder="Enter mobile no" name=""id=""></td>
+  </tr>
+
+  <tr>
+    <td>Email Address</td>
+    <td><input type="text" placeholder="Enter email"name="" id=""></td>
+  </tr>
+
+  <tr>
+    <td> Address</td>
+    <td><textarea name="address"id=""></textarea></td>
+  </tr>
+  <tr>
+    <td>Occupation:</td>
+    <td><input type="text" name="" id=""></td>
+  </tr>
+  
+
+
+
+
+
+
+
+    </html>
+
